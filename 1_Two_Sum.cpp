f6b073a91380abc8ea1232fd39fe97310ed26d12
ドン・ゾル <donzoru@gmail.com>
2019-04-08 10:42:14 +0800

    Problem 1

--- 1_Two_Sum.cpp
+++ 1_Two_Sum.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> hash;
-        vector<int> ans;
-        ans.clear();
+        map<int,int> hash;
         for(int i=0;i<nums.size();++i){
-            if(hash.find(target-nums[i]) != hash.end()){
-                ans.push_back(hash[target-nums[i]]);
-                ans.push_back(i);
-                return ans;
-            }
+            if(hash.find(target-nums[i]) != hash.end())
+                return {hash[target-nums[i]],i};
             hash[nums[i]]=i;
         }
-        ans.push_back(-1);
-        ans.push_back(-1);
-        return ans;
+        return {-1,-1};
     }
 };
